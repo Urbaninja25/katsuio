99610ec49b89003527b2e976a9bf0ca5e78785f9
Urbaninja25 <nbeka17@freeuni.edu.ge>
2023-12-14 14:56:19 +0400

    vercel deployment issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState, useEffect } from "react";
-import { useUser, UserButton, SignInButton } from "@clerk/nextjs";
+import { useUser, UserButton } from "@clerk/nextjs";
 import { Button } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
 import { type NextPage } from "next";
@@ -341,26 +341,7 @@ const Home: NextPage = () => {
             <UserButton afterSignOutUrl="/" />
           </>
         ) : (
-          <div className="flex min-h-screen items-center justify-center bg-gray-700 px-6 py-12 lg:px-8">
-            <div className="w-full text-center">
-              <div className="flex flex-col items-center justify-center">
-                <div className="mb-4 mt-8 flex items-center justify-center sm:flex-row sm:justify-center">
-                  <div className="mb-4 sm:mb-0 sm:mr-4">
-                    <Image
-                      width={700}
-                      height={100}
-                      src="/images/katsuioai-high-resolution-logo-transparent.png"
-                      alt="Your Company"
-                      priority
-                    />
-                  </div>
-                  <div className="whitespace-nowrap rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-                    <SignInButton />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          <SignInPage />
         )}
       </div>
     </PageLayout>
